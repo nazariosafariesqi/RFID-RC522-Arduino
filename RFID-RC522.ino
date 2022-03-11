97829dcd97ad1740d551d99fd182d3e5e6791dd9
Nazario Safariesqi <nazariotyo@gmail.com>
2022-03-11 19:30:43 +0700

    Looping Read Card

---
+++ RFID-RC522.ino
@@ -0,0 +1,27 @@
+#include <SPI.h>
+#include <MFRC522.h>
+#define SS_PIN 53
+#define RST_PIN 49
+
+MFRC522 Kartu(SS_PIN, RST_PIN);
+void setup(){
+  Serial.begin(9600);
+  SPI.begin();
+  Kartu.PCD_Init();
+  Serial.println("Put your card to the reader ... ");
+}
+
+void loop(){
+  if(Kartu.PICC_IsNewCardPresent())
+  {
+    if(Kartu.PICC_ReadCardSerial()){
+      Serial.print("Tag UID:");
+      for(byte i = 0; i < mfrc522.uid.size; i++){
+          Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+          Serial.print(mfrc522.uid.uidByte[i], HEX);
+    }
+    Serial.println();
+    mfrc522.PICC_HaltA();
+  }
+}
+}
